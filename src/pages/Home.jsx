6c2cdf03138c472248ce0b5edc5b78a5163f2c8d
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2023-05-17 13:32:08 +0630

    add-next-video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import videoUrl from "../assets/mp4.mp4";
+import videoPlay from "../assets/play.mp4";
 
 const Home = ({ className }) => {
   return (
     <div className={className} style={{ background: "yellow" }}>
-      <video controls>
+      <video controls width={"50%"}>
         <source src={videoUrl} type="video/mp4"/>
+      </video>
+      <video controls width={"50%"}>
+        <source src={videoPlay} type="video/mp4"/>
       </video>
     </div>
   );
